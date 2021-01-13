9b8836e2becd3678b571edb8a167c6aeb6283fce
gans777 <animekogan@gmail.com>
2021-01-14 02:34:18 +0300

    begin avtoriz on vue

---
+++ src/components/Avtoriz.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<b-button>Button</b-button>
+  <b-button variant="danger">Button</b-button>
+  <b-button variant="success">Button</b-button>
+  <b-button variant="outline-primary">Button</b-button>
+	</div>
+</template>
+<script type="text/javascript">
+	export	default {
+
+	}
+</script>
