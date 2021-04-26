f141250fc4d1b76607546723a45f741e3c6a8172
gans777 <animekogan@gmail.com>
2021-04-26 09:35:50 +0300

    problem with point's massiv

--- src/components/MapDeficitYandex.vue
+++ src/components/MapDeficitYandex.vue
@@ -21,12 +21,13 @@
 import axios from 'axios';
 export default {
 	props:['deficit'], 
-  data: () => ({ // stop here 1504
-  
+  data() { 
+  return{
+points: [],  //stop here 2204
   coords: [47.23470683868971,
   39.72326724340817]
- 
-  }),
+ }
+  },
   mounted() {// нужене ли этот блок вообще???
  let user_login=localStorage.getItem('user_login'); 
  let user_hash=localStorage.getItem('user_hash');
@@ -37,11 +38,9 @@ export default {
      params.append('user_hash', user_hash);
      axios.post('http://avtorizmap/ajax/ajaxrequest.php', params).then(response => {console.log('ответ c базы о маркерах');
      console.log(response);
-    //response.data.forEach(function(value){
-    //let size = Object.keys(value).length;
-   // });
+
      
-     
+         
       });
 		},
 		watch: {
@@ -55,9 +54,27 @@ export default {
      params.append('user_login', user_login);
      params.append('user_hash', user_hash);
      params.append('product', this.deficit);
-     axios.post('http://avtorizmap/ajax/ajaxrequest.php', params).then(response => {console.log(response);//расставить все маркеры
-
-     
+     axios.post('http://avtorizmap/ajax/ajaxrequest.php', params).then(response => {
+console.log("расставляем маркеры");
+     console.log(response);//расставить все маркеры
+     console.log("i am after all_markers");
+     var count=0;
+      response.data.forEach(function(value){
+      count=count+1;
+       let point={};
+      point.id_point=value['id_point'];
+     //console.log("id_point="+value['id_point']);
+     point.name=value['name'];
+     console.log("name= "+point.name);
+     point.coords=[Number(value['lan']),Number(value['lng'])];
+     //console.log("lan="+value['lan'] +" lng="+value['lng']);
+   //this.points.push(point);
+   console.log(count);
+   this.points[count]=count;
+     //this.points.push(point);
+     //this.$set(this.points, 1, point);	
+     });
+    console.log(this.points);//stop here 2504
       });
 			}
 		},
